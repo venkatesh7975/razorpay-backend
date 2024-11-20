7ed3f084ecfc51f256d30cec12b3b774c8144f08
venkatesh7975 <venkateshmoyya7975@gmail.com>
2024-11-20 07:04:23 +0530

    first commit

--- database/db.js
+++ database/db.js
@@ -1,14 +1,17 @@
 import { connect } from "mongoose";
 
 const connectToMongo = async () => {
-    try {
-        await connect('mongodb://localhost:27017', {
-            dbName: "paymentGateway",
-        });
-        console.log("---***Database Connected Successfully***---")
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await connect(
+      "mongodb+srv://root:root@razorpay.emjiy.mongodb.net/?retryWrites=true&w=majority&appName=razorpay",
+      {
+        dbName: "paymentGateway",
+      }
+    );
+    console.log("---***Database Connected Successfully***---");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export default connectToMongo;
